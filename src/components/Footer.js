d876fbeed0c1d224d39c09315425a3db55651e92
nagalakshmikalluri <146856673+nagalakshmikalluri@users.noreply.github.com>
2025-10-07 12:45:28 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,12 +11,12 @@ const Footer = () => {
               Built with ❤️ by
             </span>
             <a 
-              href="https://www.linkedin.com/in/naveenkumar-kalluri-3b7709224/"
+              href="www.linkedin.com/in/nagalakshmi-kalluri"
               target="_blank"
               rel="noopener noreferrer"
               onClick={(e) => {
                 e.preventDefault();
-                window.open('https://www.linkedin.com/in/naveenkumar-kalluri-3b7709224/', '_blank', 'noopener,noreferrer');
+                window.open('www.linkedin.com/in/nagalakshmi-kalluri', '_blank', 'noopener,noreferrer');
               }}
               className="inline-flex items-center space-x-2 text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300 font-semibold transition-colors duration-200 group cursor-pointer"
             >
